ed26565c65304dc17105cca8612bb4a4ce1a9309
PauloLisboa7 <paulolisboaa7@gmail.com>
2026-01-23 09:31:36 -0300

    Projeto React Native com 3 telas: Home, Details e Profile com formulário de cadastro

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }: any) {
+  useEffect(() => {
+    console.log('HomeScreen montado');
+    return () => {
+      console.log('HomeScreen desmontado');
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao Meu Primeiro App!</Text>
+      <Button
+        title="Ir para Detalhes"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
